06494bd632ab5c799f78ccae1c12c821367807f8
fr3nm0 <efren.morales.flores@gmail.com>
2024-02-16 14:38:37 -0600

    Creando proyecto BackEnd

---
+++ BackEnd/reciclame-backend/src/test/java/com/s1316tjavanext/reciclamebackend/ReciclameBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s1316tjavanext.reciclamebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReciclameBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
