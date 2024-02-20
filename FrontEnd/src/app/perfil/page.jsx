4eae074c37af152a614e0e95f729053b85625b22
Matias Luengo <matiasluengoo95@gmail.com>
2024-02-19 22:05:33 -0300

    added: perfil page and ProfileCard component

---
+++ FrontEnd/src/app/perfil/page.jsx
@@ -0,0 +1,10 @@
+import BlogPost from "../../components/BlogPost";
+
+export default function PerfilPage() {
+    return(
+        <section className="w-11/12 mx-auto">
+            <p>Publicaciones</p>
+            <BlogPost/>
+        </section>
+    )
+}
