6bfd23506e7d9dd0dd8a4d38313636f9a036b88b
Matias Luengo <matiasluengoo95@gmail.com>
2024-02-20 17:43:32 -0300

    add: fav and config icons

--- FrontEnd/src/components/BlogPost.jsx
+++ FrontEnd/src/components/BlogPost.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image"
 import img from '../assets/unsplash_6ZGEQb-CxIo.png'
+import FavIcon from '../components/Icons/FavIcon'
 
 export default function BlogPost() {
   return (
@@ -12,11 +13,16 @@ export default function BlogPost() {
 
 export function BlogArticle({tittle}){
     return(
-        <article className="flex flex-col gap-3 cursor-pointer hover:scale-105">
-            <div className="w-full h-full flex justify-center items-center">
-            <Image src={img} alt="" width={370} height={280} className="bg-gray-300 rounded-[11px]" />
-            </div>
-            <p className="text-[16px] font-[500]">{tittle}</p>
-        </article>
+        <div className="relative group">
+            <article className="flex flex-col gap-3 cursor-pointer group-hover:scale-105">
+                <div className="w-full h-full flex justify-center items-center">
+                <Image src={img} alt="" width={370} height={280} className="bg-gray-300 rounded-[11px]" />
+                </div>
+                <p className="text-[16px] font-[500]">{tittle}</p>
+            </article>
+            <button className="hidden group-hover:block absolute top-4 right-4 ">
+                <FavIcon width="25" height="24" />
+            </button>
+        </div>
     )
 }
