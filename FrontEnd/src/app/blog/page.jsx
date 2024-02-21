537206e8ec0476d243d7e13a62c30fd40b94f162
Matias Luengo <matiasluengoo95@gmail.com>
2024-02-21 20:46:39 -0300

    add: query search function in blog page

--- FrontEnd/src/app/blog/page.jsx
+++ FrontEnd/src/app/blog/page.jsx
@@ -6,15 +6,16 @@ import React, { useContext } from "react";
 import { Context } from "../../context/ContextProvider";
 import { useRouter } from "next/navigation";
 
-export default function BlogPage() {
+export default function BlogPage({searchParams}) {
+    const queryParam = searchParams?.search || ''
     const { isLogged } = useContext(Context);
     const router = useRouter();
 
     return(<>
         {isLogged ? (
             <section className="mt-28 w-11/12 mx-auto">
-                <InputSearch placeHolder={'Busca noticia'}/>
-                <BlogPost/>
+                <InputSearch query={'search'} placeHolder={'Busca noticia'}/>
+                <BlogPost query={queryParam} />
             </section>
         ) : (
             router.push("/login")
