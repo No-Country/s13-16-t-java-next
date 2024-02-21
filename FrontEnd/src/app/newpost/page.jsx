a270d9fa61bf685ac9d6f1db0efd436c48e09a0a
Cristian Mozetich <cristianmozetich@gmail.com>
2024-02-21 21:24:37 +0000

    new post

--- FrontEnd/src/app/newpost/page.jsx
+++ FrontEnd/src/app/newpost/page.jsx
@@ -3,25 +3,19 @@ import { ChevronDownIcon } from "@/src/components/Icons";
 
 export default function NewPost() {
   return (
-    <div className="m-auto flex h-full mt-20 items-center justify-center">
+    <div className="m-auto mt-20 flex h-full items-center justify-center">
       <label className="dark:hover:bg-bray-800 m-2 hidden h-72 w-96 cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 md:m-12 md:block dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600">
         <div className="flex flex-col items-center justify-center pb-6 pt-5">
           <svg
-            className="mb-3 h-10 w-10 text-gray-400"
+            viewBox="0 0 434 381"
             fill="none"
-            stroke="currentColor"
-            viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            <path d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+            <path
+              d="M158.147 90.1466C150.333 97.9532 150.333 110.527 150.333 135.667V162.333C150.333 187.473 150.333 200.047 158.147 207.853C165.953 215.667 178.527 215.667 203.667 215.667H230.333C255.473 215.667 268.047 215.667 275.853 207.853C283.667 200.047 283.667 187.473 283.667 162.333V135.667C283.667 116.733 283.667 104.933 280.333 96.9266V182.333C277.164 182.334 274.025 181.71 271.097 180.497C268.169 179.285 265.508 177.507 263.267 175.267L258.253 170.253C253.44 165.44 251.04 163.04 248.313 162.027C245.316 160.912 242.018 160.912 239.02 162.027C236.293 163.04 233.887 165.44 229.087 170.253L228.327 171.007C224.427 174.907 222.473 176.86 220.4 177.22C218.79 177.504 217.131 177.186 215.74 176.327C213.953 175.22 212.867 172.68 210.687 167.607L210.333 166.78C205.333 155.113 202.84 149.287 198.48 147.1C196.284 145.999 193.827 145.526 191.38 145.733C186.52 146.147 182.04 150.633 173.067 159.6L160.333 172.333V88.2466C159.557 88.8233 158.826 89.4585 158.147 90.1466Z"
+              fill="#BDBDBD"
+            />
           </svg>
-          <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-            <span className="font-semibold">Click to upload</span> or drag and
-            drop
-          </p>
-          <p className="text-xs text-gray-500 dark:text-gray-400">
-            SVG, PNG, JPG or GIF (MAX. 800x400px)
-          </p>
         </div>
         <input id="dropzone-file" type="file" className="hidden" />
       </label>
@@ -29,13 +23,13 @@ export default function NewPost() {
       <form className="m-2 md:m-12">
         <div className="flex flex-col justify-center">
           <input
-            className="input-form m-2 w-96 hover:border-secondary-violet focus:outline-secondary-violet"
+            className="input-form m-2 w-full hover:border-secondary-violet focus:outline-secondary-violet md:w-96"
             type="text"
             placeholder="Título"
           />
           <div className="relative">
             <select
-              className="input-form m-2 w-96 hover:border-secondary-violet focus:outline-secondary-violet"
+              className="input-form m-2 w-full hover:border-secondary-violet focus:outline-secondary-violet md:w-96"
               type="select"
               placeholder="Categoría"
             >
@@ -51,7 +45,7 @@ export default function NewPost() {
             <ChevronDownIcon className="chevron-down-icon mr-2 p-1" />
           </div>
           <textarea
-            className="input-form m-2 max-h-80 min-h-40 w-96 hover:border-secondary-violet focus:outline-secondary-violet"
+            className="input-form m-2 max-h-80 min-h-40 w-full resize-none hover:border-secondary-violet focus:outline-secondary-violet md:w-96"
             type="text"
             placeholder="Desripción"
           />
@@ -59,27 +53,21 @@ export default function NewPost() {
         <label className="dark:hover:bg-bray-800 m-2 block h-72 w-96 cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed border-gray-300 bg-gray-50 hover:bg-gray-100 md:m-12 md:hidden dark:border-gray-600 dark:bg-gray-700 dark:hover:border-gray-500 dark:hover:bg-gray-600">
           <div className="flex flex-col items-center justify-center pb-6 pt-5">
             <svg
-              className="mb-3 h-10 w-10 text-gray-400"
+              viewBox="0 0 434 381"
               fill="none"
-              stroke="currentColor"
-              viewBox="0 0 24 24"
               xmlns="http://www.w3.org/2000/svg"
             >
-              <path d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"></path>
+              <path
+                d="M158.147 90.1466C150.333 97.9532 150.333 110.527 150.333 135.667V162.333C150.333 187.473 150.333 200.047 158.147 207.853C165.953 215.667 178.527 215.667 203.667 215.667H230.333C255.473 215.667 268.047 215.667 275.853 207.853C283.667 200.047 283.667 187.473 283.667 162.333V135.667C283.667 116.733 283.667 104.933 280.333 96.9266V182.333C277.164 182.334 274.025 181.71 271.097 180.497C268.169 179.285 265.508 177.507 263.267 175.267L258.253 170.253C253.44 165.44 251.04 163.04 248.313 162.027C245.316 160.912 242.018 160.912 239.02 162.027C236.293 163.04 233.887 165.44 229.087 170.253L228.327 171.007C224.427 174.907 222.473 176.86 220.4 177.22C218.79 177.504 217.131 177.186 215.74 176.327C213.953 175.22 212.867 172.68 210.687 167.607L210.333 166.78C205.333 155.113 202.84 149.287 198.48 147.1C196.284 145.999 193.827 145.526 191.38 145.733C186.52 146.147 182.04 150.633 173.067 159.6L160.333 172.333V88.2466C159.557 88.8233 158.826 89.4585 158.147 90.1466Z"
+                fill="#BDBDBD"
+              />
             </svg>
-            <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
-              <span className="font-semibold">Click to upload</span> or drag and
-              drop
-            </p>
-            <p className="text-xs text-gray-500 dark:text-gray-400">
-              SVG, PNG, JPG or GIF (MAX. 800x400px)
-            </p>
           </div>
           <input id="dropzone-file" type="file" className="hidden" />
         </label>
         <div className="w-full text-end">
           <Link
-            className="m-2 w-28 rounded-3xl border border-secondary-violet p-2 text-center text-secondary-violet"
+            className="m-2 lg:w-28 w-full rounded-3xl border border-secondary-violet p-2 text-center text-secondary-violet"
             href={""}
             type="submit"
           >
@@ -87,7 +75,7 @@ export default function NewPost() {
           </Link>
           <Link
             type="submit"
-            className="m-2 w-28 rounded-3xl bg-accent-yellow p-2 text-center"
+            className="m-2 lg:w-28 w-full rounded-3xl bg-accent-yellow p-2 text-center"
             href={""}
           >
             Publicar
