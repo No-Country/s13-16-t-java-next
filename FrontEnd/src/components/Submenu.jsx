12d87d1486ce66e017d0985994a3dc6e690c9e61
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2024-02-21 19:16:24 +0100

    logOut session

--- FrontEnd/src/components/Submenu.jsx
+++ FrontEnd/src/components/Submenu.jsx
@@ -1,10 +1,18 @@
 import Link from "next/link";
 import Image from "next/image"
-import { useState } from "react";
+import { useState, useContextgit  } from "react";
+import { Context } from "../context/ContextProvider";
+
 
 export default function Submenu() {
   const [showMenu, setShowMenu] = useState(false);
+
+  const {setlogged} = useContext(Context)
+  const logOut = () => {
+      setlogged(false)
+  }
   return (
+  
     <>
       <Image
         width={60}
@@ -20,7 +28,7 @@ export default function Submenu() {
           <Link href={"/profile"} className="hover:border-b-2 border-secondary-violet"> Perfil</Link>
           <Link href={""} className="hover:border-b-2 border-secondary-violet"> Donaciones</Link>
           <Link href={"/configuracion"}  className="hover:border-b-2 border-secondary-violet"> Configuración</Link>
-          <Link href={"/login"} className="hover:border-b-2 border-secondary-violet"> Cerrar Sesion</Link>
+          <Link href={"/"} onClick={logOut} className="hover:border-b-2 border-secondary-violet"> Cerrar Sesion</Link>
         </div>
       ) : null}
     </>
