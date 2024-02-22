33f51da33b12b3f48b32f7f208a3d9cce632e8d8
lihuelmujica <lihuelmujica@gmail.com>
2024-02-21 21:29:50 -0300

    docs: add swagger documentation to Profile entity

--- BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/entity/Profile.java
+++ BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/entity/Profile.java
@@ -1,5 +1,6 @@
 package com.s1316tjavanext.reciclamebackend.entity;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,15 +15,25 @@ import java.util.UUID;
 @NoArgsConstructor
 @Entity
 @Table(name = "profiles")
+@Schema(name = "Profile", description = "Profile of a user")
 public class Profile implements Serializable {
 
+    @Schema(name = "id", description = "Unique identifier of the profile", example = "123e4567-e89b-12d3-a456-426614174000")
     @Id
     private UUID id;
     @Column(name = "given_name")
+
+    @Schema(name = "name", description = "Name of the user", example = "John")
     private String name;
+
+    @Schema(name = "last_name", description = "Last name of the user", example = "Doe")
     @Column(name = "family_name")
     private String lastName;
+
+    @Schema(name = "email", description = "Email of the user", example = "1234567")
     @Column(name = "photo_id")
     private String photoId;
+
+    @Schema(name = "bio", description = "Biography of the user", example = "I'm a software engineer")
     private String bio;
 }
