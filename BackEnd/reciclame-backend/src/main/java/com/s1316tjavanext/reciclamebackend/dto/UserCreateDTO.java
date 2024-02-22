ecd4eb6834ac916cfeb8352adf0e4023fe505fec
lihuelmujica <lihuelmujica@gmail.com>
2024-02-21 21:37:58 -0300

    fix: add @JsonAllias

--- BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/dto/UserCreateDTO.java
+++ BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/dto/UserCreateDTO.java
@@ -1,5 +1,7 @@
 package com.s1316tjavanext.reciclamebackend.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -7,11 +9,20 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "UserCreateDTO", description = "Data Transfer Object to create a user")
 public class UserCreateDTO {
+    @Schema(name = "name", description = "Name of the user", example = "John")
     private String name;
+    @Schema(name = "last_name", description = "Last name of the user", example = "Doe")
+    @JsonAlias("last_name")
     private String lastName;
+    @Schema(name = "email", description = "Email of the user", example = "1234567")
     private String email;
+    @Schema(name = "password", description = "Password of the user", example = "1234567")
     private String password;
+    @Schema(name = "phone", description = "Phone of the user", example = "1234567")
     private String phone;
+    @Schema(name = "location_id", description = "Location of the user", example = "1")
+    @JsonAlias("location_id")
     private int locationId;
 }
--- BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/entity/Profile.java
+++ BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/entity/Profile.java
@@ -1,5 +1,6 @@
 package com.s1316tjavanext.reciclamebackend.entity;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
@@ -22,16 +23,17 @@ public class Profile implements Serializable {
     @Id
     private UUID id;
     @Column(name = "given_name")
-
     @Schema(name = "name", description = "Name of the user", example = "John")
     private String name;
 
     @Schema(name = "last_name", description = "Last name of the user", example = "Doe")
     @Column(name = "family_name")
+    @JsonAlias("last_name")
     private String lastName;
 
     @Schema(name = "email", description = "Email of the user", example = "1234567")
     @Column(name = "photo_id")
+    @JsonAlias("photo_id")
     private String photoId;
 
     @Schema(name = "bio", description = "Biography of the user", example = "I'm a software engineer")
