2a55edb52e9687751bfbf87e5e450d9ddf0c8dd9
fr3nm0 <efren.morales.flores@gmail.com>
2024-02-22 20:05:45 -0600

    Agregando atributo post

--- BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/entity/Profile.java
+++ BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/entity/Profile.java
@@ -38,4 +38,7 @@ public class Profile implements Serializable {
 
     @Schema(name = "bio", description = "Biography of the user", example = "I'm a software engineer")
     private String bio;
+
+//    @OneToOne
+//    private Post post;
 }
