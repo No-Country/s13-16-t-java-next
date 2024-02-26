530035433f1700980fcfd04294d674434548d326
Jesus Monroy <jdmonroyg@unal.edu.co>
2024-02-26 17:23:00 -0500

    Eliminando el repo de front de nuestra rama back para evitar conflictos

--- FrontEnd/src/app/Providers.jsx
+++
@@ -1,5 +0,0 @@
-import ContextProvider from "../context/ContextProvider";
-
-export function Providers({ children }) {
-  return <ContextProvider>{children}</ContextProvider>;
-}
--- FrontEnd/src/components/Icons/index.js
+++
@@ -1,3 +0,0 @@
-export { default as SearchIcon } from "./SearchIcon";
-export { default as AddIcon } from "./AddIcon";
-export { default as ChevronDownIcon } from "./ChevronDownIcon";
