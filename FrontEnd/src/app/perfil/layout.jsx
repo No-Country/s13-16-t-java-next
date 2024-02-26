108aef58febe1fbb78a08f0b072f6f5b42b1bc0d
Matias Luengo <matiasluengoo95@gmail.com>
2024-02-26 15:25:42 -0300

    saving advance

--- FrontEnd/src/app/perfil/layout.jsx
+++ FrontEnd/src/app/perfil/layout.jsx
@@ -1,25 +1,37 @@
-'use client';
+"use client";
 
-import ProfileCard from '../../components/ProfileCard'
-import Image from "next/image"
-import { useRouter } from 'next/navigation';
-import {Context} from '../../context/ContextProvider';
-import { useContext } from 'react';
-import {ProfileCardSkeleton} from '../../components/skeletons'
+import ProfileCard from "../../components/ProfileCard";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Context } from "../../context/ContextProvider";
+import { useContext, useEffect, useState } from "react";
 
-export default function Layout({children}) {
+export default function Layout({ children }) {
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+  if (!mounted) return null;
+
   const router = useRouter();
-  const {isLogged} = useContext(Context)
+  const { isLogged } = useContext(Context);
 
-  return (<>
-    {isLogged? (
-      <section className="mt-16 w-full mx-auto">
-        <Image src="" alt="" height={30} width={100} layout="responsive" className="bg-primary-green min-h-72" />
-        <ProfileCard/>
-        {children}
-      </section>
-    ):(
-      router.push('/login')
-    )}
-  </>);
+  return (
+    <>
+      {isLogged ? (
+        <section className="mx-auto mt-16 w-full">
+          <Image
+            src=""
+            alt=""
+            height={30}
+            width={100}
+            layout="responsive"
+            className="min-h-72 bg-primary-green"
+          />
+          <ProfileCard />
+          {children}
+        </section>
+      ) : (
+        router.push("/login")
+      )}
+    </>
+  );
 }
