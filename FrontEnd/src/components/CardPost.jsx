0b5c39ba472111f8945b511dfa1aac1954acc018
Cristian Mozetich <cristianmozetich@gmail.com>
2024-02-27 20:33:45 +0000

    heart publicacion

--- FrontEnd/src/components/CardPost.jsx
+++ FrontEnd/src/components/CardPost.jsx
@@ -1,22 +1,36 @@
 import Image from "next/image";
 import Link from "next/link";
+import HeartLike from "./Icons/HeartLike";
+import { useState } from "react";
 
 export default function CardPost({ publication }) {
   console.log(publication);
+
+  const [liked, setLiked] = useState(false);
+  const handleClickLike = () => {
+    setLiked(!liked);
+  };
   return (
-    <Link href={`/publicaciones/1`}>
-      <div className="flex flex-col ">
-        <div className="flex h-[200px] w-[250px] items-center justify-center object-cover ">
-          <Image
-            src={publication.image}
-            alt=""
-            width={250}
-            height={200}
-            className="rounded-[11px] bg-gray-300 "
-          />
-        </div>
-        <p className="text-[16px] font-[500]">{publication.title}</p>
+    <>
+      <div>
+        <Link href={`/publicaciones/1`}>
+          <div className="flex flex-col ">
+            <div className="flex h-[200px] w-[250px] items-center justify-center object-cover ">
+              <Image
+                src={publication.image}
+                alt=""
+                width={250}
+                height={200}
+                className="rounded-[11px] bg-gray-300 "
+              />
+            </div>
+            <p className="text-[16px] font-[500]">{publication.title}</p>
+          </div>
+        </Link>
+        <button onClick={handleClickLike}>
+          <HeartLike fill={liked ? "red" : "gray"} />
+        </button>
       </div>
-    </Link>
+    </>
   );
 }
