acdd2d701554bca9114ee3d214264326d491aab0
Matias Luengo <matiasluengoo95@gmail.com>
2024-03-01 17:39:47 -0300

    add: filter publication by title

---
+++ FrontEnd/src/components/explorar/CardsContainer.jsx
@@ -0,0 +1,17 @@
+import CardPost from "../CardPost";
+import { getPublicationsByTitle, getAllPublications } from "@/src/lib/api";
+
+export default async function CardsContainer({ query }) {
+  let publications = [];
+  query == ""
+    ? (publications = await getAllPublications())
+    : (publications = await getPublicationsByTitle(query));
+
+  return (
+    <div className="my-10 flex flex-wrap justify-center gap-4 p-2  md:mx-auto md:columns-6">
+      {publications?.map((publication) => (
+        <CardPost key={publication.id} publication={publication} />
+      ))}
+    </div>
+  );
+}
---
+++ FrontEnd/src/components/explorar/SearchPublication.jsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { SearchIcon } from "../Icons";
+import { useSearchParams, usePathname, useRouter } from "next/navigation";
+import { useDebouncedCallback } from "use-debounce";
+
+export default function SearchPublication({ query }) {
+  const searchParams = useSearchParams();
+  const pathName = usePathname();
+  const { replace } = useRouter();
+
+  const handleSearch = useDebouncedCallback((filtered) => {
+    const params = new URLSearchParams(searchParams);
+    filtered ? params.set(query, filtered) : params.delete(query);
+    replace(`${pathName}?${params.toString()}`);
+  }, 400);
+
+  return (
+    <label
+      id="input-search"
+      className="mx-auto flex w-full max-w-[850px] items-center gap-4 text-pretty rounded-full border-2 border-solid border-[#6D6D6D] px-4 py-2"
+    >
+      <SearchIcon className="aspect-square w-[1.875rem] fill-[#6D6D6D]" />
+      <input
+        onChange={(event) => handleSearch(event.target.value)}
+        defaultValue={searchParams.get(query)?.toString()}
+        className="w-full border-none bg-transparent text-[#6D6D6D] outline-none placeholder:text-[#6D6D6D]"
+        type="search"
+        name="search"
+        id="input-search"
+        placeholder="Busca materiales (ej: papel, plástico, etc)"
+      />
+    </label>
+  );
+}
