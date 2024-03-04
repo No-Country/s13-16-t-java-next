630cdeedf11500c610506f663cc68943f18a3033
Nicolas-Ortiz <nicortiz1234@gmail.com>
2024-03-04 12:56:41 -0300

    Create new validations

--- BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/dto/UserRequestDTO.java
+++ BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/dto/UserRequestDTO.java
@@ -3,30 +3,44 @@ package com.s1316tjavanext.reciclamebackend.dto;
 import jakarta.validation.constraints.*;
 import org.hibernate.validator.constraints.Length;
 
+import static com.s1316tjavanext.reciclamebackend.util.Constants.INVALID_BIRTHDAY;
+import static com.s1316tjavanext.reciclamebackend.util.Constants.INVALID_EMAIL;
+import static com.s1316tjavanext.reciclamebackend.util.Constants.INVALID_LENGTH_NAME;
+import static com.s1316tjavanext.reciclamebackend.util.Constants.INVALID_LENGTH_PHONE_NUMBER;
+import static com.s1316tjavanext.reciclamebackend.util.Constants.INVALID_LOCATION_ID;
+import static com.s1316tjavanext.reciclamebackend.util.Constants.INVALID_NAME;
+import static com.s1316tjavanext.reciclamebackend.util.Constants.INVALID_PASSWORD;
+import static com.s1316tjavanext.reciclamebackend.util.Constants.INVALID_PHONE_NUMBER;
+
 import java.util.Date;
-import java.util.UUID;
 
 public record UserRequestDTO(
-        @NotBlank
-        @Min(3)
+        @NotBlank(message = INVALID_NAME)
+        @Length(min = 3, message = INVALID_LENGTH_NAME)
         String name,
-        @NotBlank
+
+        @NotBlank(message = INVALID_NAME)
+        @Length(min = 3, message = INVALID_LENGTH_NAME)
         String lastName,
-        @NotBlank
-        @Email
+
+        @NotBlank(message = INVALID_EMAIL)
+        @Email(message = INVALID_EMAIL)
         String email,
-        @NotBlank
+
+        @NotBlank(message = INVALID_PHONE_NUMBER)
+        @Length(min = 9, max = 30, message = INVALID_LENGTH_PHONE_NUMBER)
         String phone,
-        @NotBlank
-        @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)[a-zA-Z0-9.!@#$&*%_\\-=]+$")
-        @Length(min = 8)
+
+        @NotBlank(message = INVALID_PASSWORD)
+        @Pattern(regexp = "^(?=.*[A-Z])(?=.*\\d)[a-zA-Z0-9.!@#$&*%_\\-=]+$", message = INVALID_PASSWORD)
+        @Length(min = 8, message = INVALID_PASSWORD)
         String password,
 
-        @Positive
+        @Positive(message = INVALID_LOCATION_ID)
         @Max(2410)
         int location_id,
 
-        @NotNull
+        @NotNull(message = INVALID_BIRTHDAY)
         Date birthdate
 ) {
 }
--- BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/util/Constants.java
+++ BackEnd/reciclame-backend/src/main/java/com/s1316tjavanext/reciclamebackend/util/Constants.java
@@ -9,14 +9,19 @@ public class Constants {
     public final static String REGEX_DESCRIPTION = "[a-zA-Z0-9áéíóúüñÁÉÍÓÚÜÑ!¡¿?,.\\-_ ]{3,500}";
     public final static String INVALID_CHARACTERS = "El campo contiene caracteres inválidos";
 
+    /* Validaciones para User */
+    public final static String INVALID_NAME = "El nombre o apellido no es valido";
+    public final static String INVALID_LENGTH_NAME = "La longitud del nombre o apellido debe ser mayor a 3";
     public final static String INVALID_EMAIL = "El email no es valido";
 
-    public final static String INVALID_PASSWORD = """
-             El campo debe ser mínimo de 8 caracteres,
-             al menos una letra mayúscula,
-             y opcionalmente solo se permite los siguientes caracteres especiales:
-             .!@#$&*%_-=
-            """;
+    public final static String INVALID_PASSWORD = 
+            "La contraseña debe ser mínimo de 8 caracteres," + 
+            "al menos una letra mayúscula, " + 
+            "y opcionalmente solo se permite los siguientes caracteres especiales: " + 
+            ".!@#$&*%_-=''";
 
     public final static String INVALID_PHONE_NUMBER = "El numero de telefono no es valido";
+    public final static String INVALID_LENGTH_PHONE_NUMBER = "La longitud del numero de telefono no es valida";
+    public final static String INVALID_LOCATION_ID = "La localidad ingresada no es valida";
+    public final static String INVALID_BIRTHDAY = "La fecha de nacimiento no es valida";
 }
