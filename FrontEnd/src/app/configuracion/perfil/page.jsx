311bb64f04a87840c6b1422122574e6d583cf199
Lohana Orellano <lohanaorellano_lsi@hotmail.comgit>
2024-03-05 08:09:04 +0100

    route configuration

--- FrontEnd/src/app/configuracion/perfil/page.jsx
+++ FrontEnd/src/app/configuracion/perfil/page.jsx
@@ -2,10 +2,15 @@
 import React, { useState } from "react";
 import { AddIcon } from "@/src/components/Icons";
 import Image from "next/image";
+import { toast } from "sonner";
+import Link from "next/link";
+
 
 export default function ConfigurationPage() {
   const [imagePreview, setImagePreview] = useState(null);
+  const [selectedCategories, setselectedCategories] = React.useState([]);
   const [, setSelectedFile] = useState({});
+  const categories = ["Madera", "Vidrio", "Piedra", "Metal", "Plastico"];
   const handleChange = (e) => {
     const file = e.target.files[0];
     setSelectedFile(file);
@@ -20,8 +25,23 @@ export default function ConfigurationPage() {
       setImagePreview("/image/profileComent.png");
     }
   };
+
+  const handleCategoriaClick = (categoria) => {
+    if (
+      selectedCategories.length === 0 &&
+      !selectedCategories.includes(categoria)
+    ) {
+      setselectedCategories([categoria]);
+    } else if (selectedCategories.includes(categoria)) {
+      setselectedCategories(selectedCategories.filter((c) => c !== categoria));
+    } else if (selectedCategories.length < 3) {
+      setselectedCategories([...selectedCategories, categoria]);
+    } else {
+      toast.error("Solo se pueden seleccionar hasta tres categorías");
+    }
+  };
   return (
-    <section className="mt-[65px] min-h-dvh bg-red-500">
+    <section className=" mx-auto mt-[65px] min-h-dvh p-3 md:max-w-[70%] ">
       <div>
         <h2 className="p-3 text-2xl">Editar Perfil</h2>
         <form action="">
@@ -63,14 +83,11 @@ export default function ConfigurationPage() {
               className={`input-form  "focus:outline-wrong"  "focus:outline-secondary-violet"}`}
               placeholder="+34 123 456 789"
               id="phone"
-          
               maxLength={14}
               onChange={(e) => {
                 e.target.value = e.target.value.replace(/[^0-9]/g, "");
               }}
             />
-           
-           
           </div>
           <div className="relative flex w-full flex-col justify-center gap-1 px-4 py-2">
             <label className="capitalize">Localidad</label>
@@ -83,7 +100,39 @@ export default function ConfigurationPage() {
           </div>
           <div className="p-3">
             <h3>Tus intereses</h3>
-            <p className="text-sm text-gray-400">Elige hasta 3 opciones:</p>
+            <p className="p- mb-2 text-sm text-gray-400">
+              Elige hasta 3 opciones:
+            </p>
+            <div className="grid grid-cols-2 gap-3 md:grid-cols-3 lg:grid-cols-4 ">
+              {categories.map((category, index) => (
+                <button
+                  className={`flex h-10 items-center  justify-center rounded-[20px] border px-5 py-3 leading-5 lg:h-12 ${
+                    selectedCategories.includes(category)
+                      ? "border-secondary-violet bg-secondary-violet text-white"
+                      : "border-secondary-violet  text-secondary-violet"
+                  }`}
+                  type="button"
+                  key={index}
+                  onClick={() => handleCategoriaClick(category)}
+                >
+                  <span>{category}</span>
+                </button>
+              ))}
+            </div>
+            <div className="mt-10 mb-5 flex w-full justify-between gap-2 lg:col-span-5 lg:mt-40 lg:justify-start">
+              <Link
+                className=" w-full rounded-full border border-primary-green px-4 py-2 text-center text-primary-green lg:w-1/4 "
+                href={"/"}
+              >
+                Cancelar
+              </Link>
+              <button
+                className=" w-full rounded-full bg-primary-green px-4 py-2 text-white lg:w-1/4  "
+                type="submit"
+              >
+                Guardar
+              </button>
+            </div>
           </div>
         </form>
       </div>
