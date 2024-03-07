df7f57ff249e09844aa72154ab0795d85c10139b
Matias Luengo <matiasluengoo95@gmail.com>
2024-03-07 09:07:37 -0300

    fix: update charge pubs

--- FrontEnd/src/components/explorar/CardsContainer.jsx
+++ FrontEnd/src/components/explorar/CardsContainer.jsx
@@ -2,7 +2,8 @@ import GetProfileId from "@/src/app/explorar/GetProfileId";
 import { getPublicationsByTitle, getAllPublications } from "@/src/lib/api";
 
 export default async function CardsContainer({ query }) {
-  let publications = await getAllPublications();
+  let publications = [];
   if (query) publications = await getPublicationsByTitle(query);
+  else publications = await getAllPublications();
   return <GetProfileId publications={publications} />;
 }
