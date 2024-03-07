df263c1ccefa9119b011090c1a9d184fff0f1cd7
Matias Luengo <matiasluengoo95@gmail.com>
2024-03-07 08:50:06 -0300

    fix: get publications in explorar

--- FrontEnd/src/components/explorar/CardsContainer.jsx
+++ FrontEnd/src/components/explorar/CardsContainer.jsx
@@ -2,10 +2,7 @@ import GetProfileId from "@/src/app/explorar/GetProfileId";
 import { getPublicationsByTitle, getAllPublications } from "@/src/lib/api";
 
 export default async function CardsContainer({ query }) {
-  let publications = [];
-  query == ""
-    ? (publications = await getAllPublications())
-    : (publications = await getPublicationsByTitle(query));
-
+  let publications = await getAllPublications();
+  if (query) publications = await getPublicationsByTitle(query);
   return <GetProfileId publications={publications} />;
 }
